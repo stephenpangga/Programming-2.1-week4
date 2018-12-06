1e32e50e5d6cb88fc17705343bb63dc61bae8982
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2018-12-06 15:31:50 +0100

    Add project files.

---
+++ Assignment01/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    class Person
+    {
+        public string Name;
+        public string City;
+        public int Age;
+    }
+}
---
+++ Assignment01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+            Console.ReadKey();//this is to keep the window up and running.
+        }
+        void Start()
+        {
+            Person people = ReadPerson();
+        }
+        Person ReadPerson()
+        {
+            Person person = new Person();
+            person.Name= ReadString("Enter your name: ");
+            Console.WriteLine($"welcome {person.Name}!");
+            person.City = ReadString("Enter City: ");
+            person.Age = ReadInt("Enter Age: ", 0);
+
+            return person;
+        }
+        string ReadString(string question)
+        {
+            Console.Write(question); // to display the question
+            return Console.ReadLine();
+        }
+        int ReadInt(string question, int number)
+        {
+            Console.Write(question);
+            string value = Console.ReadLine();
+            int age = int.Parse(value);
+            return age;
+        }
+    }
+}
