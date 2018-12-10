47013051d26bd990489c9a85ab5f1481fe84e4e2
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2018-12-10 22:06:09 +0100

    trying to understand and remake hangman game from week 3 for week 4 assignment

---
+++ Assignment02/HangmanGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    class HangmanGame
+    {
+        public string secretWord;
+        public string guessedWord;
+        public void Init(string secretWord)
+        {
+            foreach(char letter in secretWord)
+            {
+                this.secretWord = secretWord;//just to show the word according to jurek.
+                guessedWord += ".";
+            }
+        }
+    }
+}
--- Assignment02/Program02.cs
+++ Assignment02/Program02.cs
@@ -16,7 +16,31 @@ namespace Assignment02
         }
         void Start()
         {
-            
+            List<string> words = new List<string>();
+            words = ListOfWords();
+            HangmanGame hangman = new HangmanGame();
+            hangman.Init(SelectWord(words));
+            Console.WriteLine("the secret word is: " + hangman.secretWord);
+            Console.WriteLine("the guessed word is: " + hangman.guessedWord);
+        }
+        List<string> ListOfWords()
+        {
+            List<string> Lwords = new List<string>();
+            Lwords.Add("Programming");
+            Lwords.Add("Modelling");
+            Lwords.Add("WebDesign");
+            return Lwords;
+        }
+        string SelectWord(List<string> words)
+        {
+            Random rnd = new Random();//random number generator
+            int values = rnd.Next(0, 2);
+            string randomWords = words.ElementAt(values);//setting number to the random numbers from 1-3
+            return randomWords;
+        }
+        bool PlayHangman(HangmanGame hangman)
+        {
+
         }
     }
 }
